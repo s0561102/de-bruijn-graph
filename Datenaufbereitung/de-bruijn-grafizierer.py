6728f1dd39fe7b2a1ce262e6bec6f0eca886cfd1
s0561102 <s0561102@htw-berlin.de>
2021-09-20 15:47:29 +0200

    Python Script zum Aufbereiten der Testdaten

---
+++ Datenaufbereitung/de-bruijn-grafizierer.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 26 15:28:50 2021
+Source: https://towardsdatascience.com/genome-assembly-using-de-bruijn-graphs-69570efcc270
+@author: tm
+"""
+from Bio import SeqIO
+import csv
+import hashlib
+
+# funktion nimmt k-mere an
+# gibt nodes, edges und list mit start nodes zurück
+def debruijnize(k_mers):
+    # erstellen eines Sets für nodes
+    nodes = set()
+    # erstellen eines sets für nodes die kein Start sind?
+    # List für edges(als Tupel (x, y))
+    edges = []
+    for k, w in k_mers.items():
+        r1 = k[:-1] # string von der ersten [0] bis zur vorletzten Stelle des Strings
+        r2 = k[1:]  # string von zweiten [1] bis zur letzen Stelle des String
+        r1_hash = hashlib.sha224(r1.encode('utf-8')).hexdigest()
+        r2_hash = hashlib.sha224(r2.encode('utf-8')).hexdigest()
+        nodes.add(r1) # die k-1-mere werden zu node set hinzugefügt wenn sie nicht schon existieren
+        nodes.add(r2)
+        edges.append((r1,r2)) # Tupel aus nodes wird edges list hinzugefügt
+    
+    return (nodes,edges) 
+
+
+def build_k_mer(str,k):
+    return [str[i:k+i] for i in range(0,len(str)-k+1)]
+
+
+# Einlesen der Reads aus fasta Datei zu k-meren
+def read_fasta_reads_to_kmers(filename, k_mer_size):
+    k_mers = []
+    weighted = {}
+    for seq_record in SeqIO.parse(filename, "fasta"):
+        k_mers.extend(build_k_mer(str(seq_record.seq), k_mer_size))
+
+        
+    for i in k_mers:
+        if i not in weighted:
+            weighted[i] = 0
+        weighted[i] += 1           
+    return weighted
+
+
+def de_bruijn_graphizer(filename, k):
+    k_mers = {}
+    k_mers = read_fasta_reads_to_kmers(filename, k)
+    G = debruijnize(k_mers)
+    write_edges_file(G[1])
+    write_nodes_file(G[0])
+    #print_list(G[0])
+    
+    
+def write_edges_file(edges):
+    h = ['source', 'target']
+    f = open('../Visualisierung/edges.csv', 'w')
+    w = csv.writer(f)
+    w.writerow(h)
+    st = set(edges)
+    for i in list(st):        
+        w.writerow(i)
+    
+    f.close()
+    
+    
+def write_nodes_file(nodes):
+    h = ['id']
+    f = open('../Visualisierung/nodes.csv', 'w')
+    w = csv.writer(f)
+    w.writerow(h)
+    # cast set to list
+    lis = list(nodes)
+    for n in lis:
+        w.writerow([n])
+    
+    f.close()
+    
+    
+def print_list(items):
+    for i in items:
+        print(i)
+        
+    
+#print(read_fasta_reads_to_kmers("virus_perfectreads.fasta", 4))    
+    
+de_bruijn_graphizer("virus2_perfectreads.fasta", 9)
